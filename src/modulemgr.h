f3cf5048731fde3652f8cc3c5e8f8e2fe359a128
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2019-09-27 08:25:48 +0900

    Create modulemgr.h

---
+++ src/modulemgr.h
@@ -0,0 +1,22 @@
+#ifndef _PSP2_MODULEMGR_H_
+#define _PSP2_MODULEMGR_H_
+
+#include "modulemgr_types.h"
+
+SceUID _sceKernelOpenModule(const char *path, SceSize args, void *argp, SceKernelModuleOpen_t *module_open);
+
+int _sceKernelCloseModule(SceUID modid, SceSize args, void *argp, SceKernelModuleClose_t *module_close);
+
+SceUID _sceKernelLoadModule(const char *path, int flags, SceKernelLMOption *option);
+
+int _sceKernelStartModule(SceUID modid, SceSize args, void *argp, SceKernelModuleStart_t *module_start);
+
+SceUID _sceKernelLoadStartModule(const char *path, SceSize args, void *argp, SceKernelModuleLoadStart_t *module_load_start);
+
+int _sceKernelStopModule(SceUID modid, SceSize args, void *argp, SceKernelModuleStop_t *module_stop);
+
+int _sceKernelUnloadModule(SceUID modid, int flags, SceKernelULMOption *option);
+
+int _sceKernelStopUnloadModule(SceUID modid, SceSize args, void *argp, SceKernelModuleStopUnload_t *module_stop_unload);
+
+#endif /* _PSP2_MODULEMGR_H_ */
