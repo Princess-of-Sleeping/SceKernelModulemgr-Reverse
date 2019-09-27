74a12b7c612712f085b820b73e4c5b52ee6c8730
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2019-09-28 07:51:39 +0900

    Update modulemgr_types.h

--- src/modulemgr_types.h
+++ src/modulemgr_types.h
@@ -4,6 +4,25 @@
 #include <psp2kern/kernel/modulemgr.h>
 
 typedef struct {
+  char module_name[0x1C];
+} SceKernelModuleName;
+
+typedef struct {
+  SceSize size; //!< sizeof(SceKernelModuleInfo2) : 0x120
+  SceUID modid1;
+  uint32_t unk_0x08;
+  uint16_t unk_0x0C;
+  uint16_t unk_0x0E;
+  uint16_t unk_0x10;
+  uint16_t unk_0x12;
+  uint16_t unk_0x14;
+  uint16_t unk_0x16;
+  char module_name[0x100]; // offset : 0x18
+  uint32_t unk_0x118;
+  SceUID modid2;
+} SceKernelModuleInfo2_fix;
+
+typedef struct {
 	int flags;
 	int *status;
 	SceKernelLMOption *option;
