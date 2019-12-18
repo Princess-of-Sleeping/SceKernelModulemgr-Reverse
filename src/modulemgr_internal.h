7cef77c045c4cadea2e25c2e86651a22a7b0cfbd
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2019-12-18 14:27:57 +0900

    Update modulemgr_internal.h

--- src/modulemgr_internal.h
+++ src/modulemgr_internal.h
@@ -1,17 +1,72 @@
+/*
+ * PS Vita kernel module manager RE
+ * Copyright (C) 2019, Princess of Sleeping
+ */
 
 #ifndef _PSP2_KERNEL_MODULEMGR_INTERNAL_H_
 #define _PSP2_KERNEL_MODULEMGR_INTERNAL_H_
 
 #include <psp2kern/types.h>
 
-typedef struct {
-  SceSize size;   //!< this structure size (0x18)
-  SceUInt perms;  //!< probably rwx in low bits
-  void *vaddr;    //!< address in memory
-  SceSize memsz;  //!< size in memory
-  SceSize filesz; //!< original size of memsz
-  SceUInt res;    //!< unused
-} SceKernelSegmentInfo_fix;
+typedef struct SceKernelModuleInfoObjBase_t SceKernelModuleInfoObjBase_t;
+
+typedef int (* SceKernelModuleEntry)(SceSize args, void *argp);
+
+typedef struct SceModuleLibImport_t {
+
+	uint16_t size; // 0x24
+	uint16_t libver[2];
+	uint16_t entry_num_function;
+	uint16_t entry_num_variable;
+	uint16_t data_0x0A; // unused?
+	uint32_t libnid;
+
+	const char *libname;
+	void *ent1;
+	void *ent2;
+	void *ent3;
+
+	void *ent4;
+
+/*
+	int data_0x00; // 0x10024
+	int data_0x04; // 0x10008
+	int data_0x08;
+	uint32_t library_nid;
+*/
+} SceModuleLibImport_t;
+// } module_library_info_t;
+
+typedef struct SceModuleLibExport_t {
+	uint16_t size; // 0x20
+	uint16_t libver[2];
+	uint16_t entry_num_function;
+	uint16_t entry_num_variable;
+	uint16_t data_0x0A; // unused?
+	uint32_t data_0x0C; // unused?
+	uint32_t libnid;
+	const char *libname;
+	void *table_nid;
+	void *table_entry;
+} SceModuleLibExport_t;
+
+typedef struct SceModuleLibraryExportInfo_t {
+	struct SceModuleLibraryExportInfo_t *next;
+	void *data_0x04;
+	SceModuleLibExport_t *nid_info;
+	int data_0x0C; // flags?
+	int data_0x10; // ex:1
+	int data_0x14; // zero?
+	SceUID libid_kernel;
+	SceUID libid_user;
+	SceKernelModuleInfoObjBase_t *modobj;
+	int data_0x24[3]; // zero?
+	int data_0x30; // zero?
+	SceModuleLibExport_t *data_0x34; // maybe noname export
+	int data_0x38; // ex:0x40000
+
+	// maybe more
+} SceModuleLibraryExportInfo_t;
 
 typedef struct {
 	SceSize filesz;
@@ -21,8 +76,22 @@ typedef struct {
 	SceUID unk_0x10;
 } SceKernelSegmentInfoObj_t; // size (0x14)
 
-typedef struct SceKernelModuleInfoObjBase_t {
+typedef struct module_tree_t {
+	struct module_tree_t *next;
+	int data_0x04;		// ex : 0x28000 (flags?)
+	uint32_t version;	// ex : 0x03600011, -1, etc...
+	SceUID modid_kernel;
 
+	// 0x10
+	int data_0x10;
+	SceUID pid;
+	int data_0x18;		// ex : 0x1
+	const char *module_name;
+
+	// maybe more
+} module_tree_t;
+
+typedef struct SceKernelModuleInfoObjBase_t {
 	struct SceKernelModuleInfoObjBase_t *next;
 	uint16_t flags;		// ex : 0xA000
 	uint8_t type;
@@ -57,14 +126,14 @@ typedef struct SceKernelModuleInfoObjBase_t {
 	void *extabBtm;
 
 	// 0x50
-	int data_0x58;		// ex : 0x190006
-	int data_0x5C;		// ex : 0x9020D8
-	int data_0x60;
-	int data_0x64;		// ex : 0x9020D8
+	void *data_0x58;			// unk
+	SceModuleLibExport_t *data_0x5C;
+	SceModuleLibExport_t *data_0x60;	// export relation
+	SceModuleLibraryExportInfo_t *data_0x64;
 
 	// 0x60
-	int data_0x68;
-	int data_0x6C;		// ex : 0x9021E0
+	void *data_0x68;			// import relation
+	void *data_0x6C;			// unk, function table?
 	const char *path;
 	int segments_num;
 
@@ -75,101 +144,51 @@ typedef struct SceKernelModuleInfoObjBase_t {
 
 	// 0xB0
 	int data_0xB8;
-	void *module_start;
-	void *module_stop;
-	void *module_exit;
+	SceKernelModuleEntry module_start;
+	SceKernelModuleEntry module_stop;
+	SceKernelModuleEntry module_exit;
 
 	// more
 } SceKernelModuleInfoObjBase_t;
 
+typedef struct SceKernelModuleImportNonlinkedInfo_t {
+	struct SceKernelModuleImportNonlinkedInfo_t *next;
+	SceUID stubid;
+	SceModuleLibImport_t *lib_import_info;
+	int data_0x0C;
+	SceKernelModuleInfoObjBase_t *pObjBase;
+	int data_0x14;
+} SceKernelModuleImportNonlinkedInfo_t;
+
 typedef struct SceKernelModuleInfoObj_t {
-	// 0x00
 	uint32_t sce_reserved[2];
 	SceKernelModuleInfoObjBase_t obj_base;
 } SceKernelModuleInfoObj_t; // sizeof == 0x100
 
-typedef struct {
-	SceUInt size;    //!< 0x1B8 for Vita 1.x
+typedef struct SceModuleLibraryObj_t {
+	uint32_t sce_reserved[2];
+	SceModuleLibraryExportInfo_t *library_info;
 	SceUID modid;
-	uint16_t attr;
-	uint8_t minor;
-	uint8_t major;
+} SceModuleLibraryObj_t;
 
-	// 0xC
-	char module_name[0x1C];
-
-	// 0x28
-	SceUInt unk28;
-	void *module_start;
-
-	// 0x30
-	void *module_stop;
-	void *module_exit;
-
-	void *exidxTop;
-	void *exidxBtm;
-	void *extabTop;
-	void *extabBtm;
-
-	void *tlsInit;
-	SceSize tlsInitSize;
-	SceSize tlsAreaSize;
-	char path[0x100];
-	SceKernelSegmentInfo_fix segments[4];
-	SceUInt type;   //!< 6 = user-mode PRX?
-} SceKernelModuleInfo_fix_t;
-
-typedef struct module_tree_t {
-	// 0x00
-	struct module_tree_t *next;
-	int data_0x04;		// ex : 0x28000 (flags?)
-	uint32_t version;	// ex : 0x03600011, -1, etc...
+typedef struct SceModuleLibStubObj_t {
+	uint32_t sce_reserved[2];
 	SceUID modid;
+	SceSize num; // maybe non linked import num
+} SceModuleLibStubObj_t;
 
-	// 0x10
-	int data_0x10;
+typedef struct SceKernelModuleProcInfo_t {
 	SceUID pid;
-	int data_0x18;		// ex : 0x1
-	const char *module_name;
-
-	// maybe more
-} module_tree_t;
-
-typedef struct module_library_info_t {
-	int data_0x00; // 0x10024
-	int data_0x04; // 0x10008
-	int data_0x08;
-	uint32_t library_nid;
-
-	// maybe more
-} module_library_info_t;
-
-typedef struct module_info_tree_t {
-	struct module_info_tree_t *next;
-	SceUID data_0x04;
-	module_library_info_t *data_0x08;
-	int data_0x0C;
-	SceKernelModuleInfoObjBase_t *pObjBase;
-
-	// maybe more
-} module_info_tree_t;
-
-typedef struct module_tree_top_t {
-	SceUID pid;
-	void *data_0x04;	// export info?
-	int data_0x08;		// uid?
-	module_info_tree_t *module_info_tree; // non linked import info?
-	module_tree_t *module_tree;
-	int data_0x14;
-	uint16_t data_0x18;	// ex : 0x52
+	SceModuleLibraryExportInfo_t *lib_export_info;
+	SceUID data_0x08;			// uid?
+	SceKernelModuleImportNonlinkedInfo_t *import_nonlinked_list;	// non linked import info?, allocated by sceKernelAlloc
+	SceKernelModuleInfoObjBase_t *module_list;
+	SceUID proc_main_module_id;
+	uint16_t proc_module_count;
 	uint16_t inhibit_state;
 	void *data_0x1C;
 	int cpu_addr;
-	int data_0x24;		// ex : 0x1009B(modid?)
-	int data_0x28;
-	int data_0x2C;		// ex : 0x19D42EA0(void* ?)
-
-	// maybe more
-} module_tree_top_t;
+	int data_0x24;				// ex:0x1009B(modid?)
+} SceKernelModuleProcInfo_t;
 
 #endif /* _PSP2_KERNEL_MODULEMGR_INTERNAL_H_ */
