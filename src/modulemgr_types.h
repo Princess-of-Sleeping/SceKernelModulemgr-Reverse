855e37ff02fb68be033103b66f8c659e0652176b
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2019-12-18 14:28:52 +0900

    Update modulemgr_types.h

--- src/modulemgr_types.h
+++ src/modulemgr_types.h
@@ -1,7 +1,63 @@
+/*
+ * PS Vita kernel module manager RE
+ * Copyright (C) 2019, Princess of Sleeping
+ */
+
 #ifndef _PSP2_MODULEMGR_USER_TYPE_H_
 #define _PSP2_MODULEMGR_USER_TYPE_H_
 
 #include <psp2kern/kernel/modulemgr.h>
+
+typedef struct {
+  SceSize size;   //!< this structure size (0x18)
+  SceUInt perms;  //!< probably rwx in low bits
+  void *vaddr;    //!< address in memory
+  SceSize memsz;  //!< size in memory
+  SceSize filesz; //!< original size of memsz
+  SceUInt res;    //!< unused
+} SceKernelSegmentInfo_fix;
+
+typedef struct {
+	SceUInt size;    //!< 0x1B8 for Vita 1.x
+	SceUID modid;
+	uint16_t attr;
+	uint8_t minor;
+	uint8_t major;
+
+	// 0xC
+	char module_name[0x1C];
+
+	// 0x28
+	SceUInt unk28;
+	void *module_start;
+
+	// 0x30
+	void *module_stop;
+	void *module_exit;
+
+	void *exidxTop;
+	void *exidxBtm;
+	void *extabTop;
+	void *extabBtm;
+
+	void *tlsInit;
+	SceSize tlsInitSize;
+	SceSize tlsAreaSize;
+	char path[0x100];
+	SceKernelSegmentInfo_fix segments[4];
+	SceUInt type;   //!< 6 = user-mode PRX?
+} SceKernelModuleInfo_fix_t;
+
+typedef struct {
+	SceSize size;
+	uint16_t libver[2];
+	uint32_t libnid;
+	const char *libname;
+	uint16_t entry_num_function;
+	uint16_t entry_num_variable;
+	uint32_t *table_nid;
+	uint32_t *table_entry;
+} SceKernelLibraryInfo;  // size is 0x1C
 
 typedef struct {
   char s[0x1C];
@@ -9,7 +65,7 @@ typedef struct {
 
 typedef struct {
 	SceUID modid;
-	uint32_t library_nid;
+	uint32_t libnid;
 } SceKernelModuleImportNID;
 
 typedef struct {
