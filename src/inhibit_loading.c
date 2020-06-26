9310b6db007d1e3620f8f4ebc0a0a6f0c4cba2e5
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-06-26 16:53:51 +0900

    Create inhibit_loading.c

---
+++ src/inhibit_loading.c
@@ -0,0 +1,59 @@
+/*
+ * PS Vita kernel module manager RE Inhibit Loading
+ * Copyright (C) 2020, Princess of Sleeping
+ */
+
+#include <psp2/types.h>
+#include <psp2kern/kernel/sysmem.h>
+#include <psp2kern/kernel/threadmgr.h>
+#include <psp2kern/kernel/cpu.h>
+#include <string.h>
+
+#include "modulemgr_types.h"
+#include "modulemgr_internal.h"
+#include "modulemgr_common.h"
+
+/*
+ * inhibit_loading_module / func_0x81003708
+ */
+int inhibit_loading_module(uint16_t flag){
+
+	SceKernelProcessModuleInfo *pProcModuleInfo;
+	SceUID pid;
+	int cpu_suspend_intr;
+
+	if((flag & ~0x30) != 0)
+		return 0x80020005;
+
+	pid = ksceKernelGetProcessId();
+	if(pid == 0x10005)
+		return 0x8002D017;
+
+	pProcModuleInfo = getProcModuleInfo(pid, &cpu_suspend_intr);
+	if(pProcModuleInfo == NULL)
+		return 0x8002D080;
+
+	if(flag <= (pProcModuleInfo->inhibit_state & 0x30)){
+		func_0x81006e90(pProcModuleInfo, cpu_suspend_intr);
+		return 0x80020005;
+	}
+
+	pProcModuleInfo->inhibit_state &= ~0x30;
+	pProcModuleInfo->inhibit_state |= flag;
+
+	func_0x81006e90(pProcModuleInfo, (int)cpu_suspend_intr);
+
+	return 0;
+}
+
+int sceKernelInhibitLoadingModule(uint16_t flag){
+
+	int res;
+	uint32_t state;
+
+	ENTER_SYSCALL(state);
+	res = inhibit_loading_module(flag);
+	EXIT_SYSCALL(state);
+
+	return res;
+}
