3058ed3a6b03eeeb754004b7c01b02209f39840e
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-06-26 16:56:00 +0900

    Update modulemgr_common.h

--- src/modulemgr_common.h
+++ src/modulemgr_common.h
@@ -24,13 +24,13 @@ void func_0x810014d4(void);
 int func_0x81001ec4(SceUID pid);
 SceKernelModuleInfoObj_t *func_0x81001f0c(SceUID modid);
 
-int func_0x810021b8(SceUID pid);
+int release_obj(SceUID uid);
 int func_0x810021c0(SceUID pid);
-int func_0x810021d8(SceUID pid);
+int release_obj_for_user(SceUID uid);
 
-int func_0x81003708(uint16_t flag);
+int inhibit_loading_module(uint16_t flag);
 
-int func_0x810040c8(SceKernelModuleProcInfo_t *a1);
+int func_0x810040c8(SceKernelProcessModuleInfo *a1);
 int func_0x81004198(void *a1, int a2, int a3);			// yet not Reversed
 int func_0x8100428c(void *a1, int a2, int a3);			// yet not Reversed
 void *func_0x8100498c(SceUID pid, SceSize len);
@@ -46,8 +46,8 @@ int func_0x81005fec(void *a1, const void *a2);			// yet not Reversed
 //int func_0x81006cf4(int a1, int a2, int a3, void *a4);
 void *func_0x81006cf4(int a1, void *a2, const void *a3, void *a4);
 SceModuleLibraryObj_t *func_0x81006de8(SceUID pid, SceUID libid);
-SceKernelModuleProcInfo_t *get_proc_module_tree_obj_for_pid(SceUID pid, int *cpu_suspend_intr);
-int func_0x81006e90(SceKernelModuleProcInfo_t *module_tree_top, int cpu_suspend_intr);
+SceKernelProcessModuleInfo *getProcModuleInfo(SceUID pid, int *cpu_suspend_intr);
+int func_0x81006e90(SceKernelProcessModuleInfo *module_tree_top, int cpu_suspend_intr);
 int func_0x81006e9c(SceUID pid);
 
 int func_0x810070b4(void *a1);
@@ -55,7 +55,7 @@ int func_0x81007148(const char *path);
 int func_0x810071a8(void *r0);
 int get_module_library_info_export(SceUID pid, SceUID modid, uint32_t libnid, SceKernelLibraryInfo *info);
 int get_module_info(SceUID pid, SceUID modid, SceKernelModuleInfo_fix_t *info);
-int get_module_info_internal_by_addr(SceKernelModuleProcInfo_t *module_proc_info, const void *module_addr, SceKernelModuleInfoObjBase_t **dst);
+int get_module_info_internal_by_addr(SceKernelProcessModuleInfo *module_proc_info, const void *module_addr, SceKernelModuleInfoObjBase_t **dst);
 
 int syacall_init(void);
 
