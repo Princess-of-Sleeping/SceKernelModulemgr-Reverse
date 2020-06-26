3a6446d3dfc2c4147dad4979c6b5cb8084a33d91
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-06-26 16:50:26 +0900

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,17 @@
+if [ ! -d extra ]; then
+  mkdir extra
+  vita-libs-gen -c import.yml extra
+  cd extra
+  cmake .
+  make
+  cd ..
+fi
+
+if [ ! -d build ]; then
+  mkdir build
+fi
+
+cd build
+cmake ../
+make
+cd ..
