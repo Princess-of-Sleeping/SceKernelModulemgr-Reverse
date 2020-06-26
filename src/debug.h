e011910bdd038f07fa2c909bc8e53f6e9bb55c99
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-06-26 16:52:53 +0900

    Create debug.h

---
+++ src/debug.h
@@ -0,0 +1,13 @@
+/*
+ * PS Vita kernel module manager RE Debug header
+ * Copyright (C) 2020, Princess of Sleeping
+ */
+
+#ifndef _PSP2_KERNEL_MODULE_MGR_DEBUG_H_
+#define _PSP2_KERNEL_MODULE_MGR_DEBUG_H_
+
+#include "modulemgr_internal.h"
+
+void print_module_load_info(SceKernelModuleInfoObjBase_t *pObj);
+
+#endif /* _PSP2_KERNEL_MODULE_MGR_DEBUG_H_ */
