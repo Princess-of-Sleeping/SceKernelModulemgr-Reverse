76ea5c56ed3ac4cfb9f1e3f022a1bac26284c2d9
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-06-27 13:56:07 +0900

    Update debug.c

--- src/debug.c
+++ src/debug.c
@@ -11,7 +11,7 @@
 #include "modulemgr_types.h"
 #include "modulemgr_internal.h"
 
-const kernel_message_ctx load_param = {
+const SceKernelDebugMessageContext load_param = {
 	.hex_value0_hi = 0x6C6D2F8B,
 	.hex_value0_lo = 0x6DE580FF,
 	.hex_value1    = 0xFFAC508A,
@@ -20,7 +20,7 @@ const kernel_message_ctx load_param = {
 	.msg1          = NULL
 };
 
-const kernel_message_ctx load_param_one_seg = {
+const SceKernelDebugMessageContext load_param_one_seg = {
 	.hex_value0_hi = 0x6C6D2F8B,
 	.hex_value0_lo = 0x669C9853,
 	.hex_value1    = 0xFFAC508A,
@@ -35,11 +35,11 @@ const kernel_message_ctx load_param_one_seg = {
 void print_module_load_info(SceModuleInfoInternal *pInfo){
 	if(SceQafMgrForDriver_382C71E8() != 0){
 		if(pInfo->segments_num < 2){
-			ksceDebugPrintf2(0, (kernel_message_ctx *)&load_param_one_seg, "[%-27s]:text=%p(0x%08x), (no data)\n",
+			ksceDebugPrintf2(0, &load_param_one_seg, "[%-27s]:text=%p(0x%08x), (no data)\n",
 				pInfo->module_name, pInfo->segments[0].vaddr, pInfo->segments[0].memsz
 			);
 		}else{
-			ksceDebugPrintf2(0, (kernel_message_ctx *)&load_param, "[%-27s]:text=%p(0x%08x), data=%p(0x%08x/0x%08x)\n",
+			ksceDebugPrintf2(0, &load_param, "[%-27s]:text=%p(0x%08x), data=%p(0x%08x/0x%08x)\n",
 				pInfo->module_name,
 				pInfo->segments[0].vaddr, pInfo->segments[0].memsz,
 				pInfo->segments[1].vaddr, pInfo->segments[1].filesz, pInfo->segments[1].memsz
