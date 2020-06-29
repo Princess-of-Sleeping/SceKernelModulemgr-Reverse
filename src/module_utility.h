25c6b4a19867260a868b8900f29968538f9eb405
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-06-29 16:54:41 +0900

    Create module_utility.h

---
+++ src/module_utility.h
@@ -0,0 +1,27 @@
+/*
+ * PS Vita kernel module manager RE Utility header
+ * Copyright (C) 2020, Princess of Sleeping
+ */
+
+#ifndef _PSP2_KERNEL_MODULE_MGR_UTILITY_H_
+#define _PSP2_KERNEL_MODULE_MGR_UTILITY_H_
+
+#include "modulemgr_internal.h"
+
+SceModuleObject *get_module_object(SceUID modid);
+
+void *alloc_for_process(SceUID pid, SceSize len);
+void free_for_process(SceUID pid, void *ptr);
+
+int process_check(SceUID pid);
+int process_check_for_user(SceUID pid);
+
+int release_obj(SceUID uid);
+int release_obj_for_user(SceUID uid);
+
+int memcpy_to_kernel(SceUID pid, void *dst, const void *src, SceSize len);
+
+SceKernelProcessModuleInfo *getProcModuleInfo(SceUID pid, int *cpu_suspend_intr);
+int resume_cpu_intr(SceKernelProcessModuleInfo *pProcModuleInfo, int cpu_suspend_intr);
+
+#endif /* _PSP2_KERNEL_MODULE_MGR_UTILITY_H_ */
