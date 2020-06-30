0d42fe0f9115d8dea08d872d859e0623a2c75927
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-06-30 10:16:41 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -32,21 +32,8 @@
 #include "module_syscall.h"
 #include "module_search.h"
 #include "module_debug.h"
-
-int module_get_export_func(SceUID pid, const char *modname, uint32_t libnid, uint32_t funcnid, uintptr_t *func);
-
-#define HookExport(module_name, library_nid, func_nid, func_name) \
-	taiHookFunctionExportForKernel(0x10005, &func_name ## _ref, module_name, library_nid, func_nid, func_name ## _patch)
-#define HookImport(module_name, library_nid, func_nid, func_name) \
-	taiHookFunctionImportForKernel(0x10005, &func_name ## _ref, module_name, library_nid, func_nid, func_name ## _patch)
-#define HookOffset(modid, offset, thumb, func_name) \
-	taiHookFunctionOffsetForKernel(0x10005, &func_name ## _ref, modid, 0, offset, thumb, func_name ## _patch)
-
-#define HookRelease(hook_uid, hook_func_name)({ \
-	(hook_uid > 0) ? taiHookReleaseForKernel(hook_uid, hook_func_name ## _ref) : -1; \
-})
-
-#define GetExport(modname, libnid, funcnid, func) module_get_export_func(0x10005, modname, libnid, funcnid, (uintptr_t *)func)
+#include "taihen_macro.h"
+#include "debug.h"
 
 void *SceKernelModulemgr_text = NULL;
 void *SceKernelModulemgr_data = NULL;
@@ -103,14 +90,14 @@ void func_0x810014a8(void){
 	int r0 = 1;
 	int r1 = 0;
   
-	do{
+	do {
 		if((r0 & *(uint32_t *)(SceKernelModulemgr_data + 0x30)) != 0){
 			*(int *)(SceKernelModulemgr_data + r1) += 1;
 		}
 
 		r0 <<= 1;
 		r1 += 4;
-	}while(r1 != 0x30);
+	} while(r1 != 0x30);
 
 	*(uint32_t *)(SceKernelModulemgr_data + 0x30) = 0;
 
@@ -196,7 +183,7 @@ int sceIoCloseBootfs(SceUID fd){
 }
 
 /*
- * create_new_module_class / func_0x81005648 (need again RE)
+ * create_new_module_class / func_0x81005648 (need again RE?)
  */
 int create_new_module_class(SceUID pid, int flags, SceModuleObject **dst){
 
@@ -979,7 +966,6 @@ loc_8100231A:
 		goto loc_810024A6;
 
 	if((flags & 0x40000) == 0){
-
 		if (SceSysrootForKernel_6050A467(pid) == 1)
 			goto loc_81002546;
 
@@ -1303,158 +1289,6 @@ label_0x81003082:
 	goto label_0x81003024;
 }
 
-/*
-
-	SceUID res;
-
-	res = TAI_CONTINUE(SceUID, module_load_for_pid_ref, pid, path, flags, option);
-
-	ksceDebugPrintf("flags : 0x%08X\n", flags);
-
-	return res;
-*/
-tai_hook_ref_t module_load_for_pid_ref;
-SceUID module_load_for_pid_patch(SceUID pid, const char *path, int flags, SceKernelLMOption *option){
-	return module_load_for_pid(pid, path, flags, option);
-}
-
-tai_hook_ref_t module_unload_for_pid_ref;
-int module_unload_for_pid_patch(SceUID pid, SceUID modid, int flags, SceKernelULMOption *option){
-
-	int res;
-	int res_debug;
-	char module_name[0x1C];
-	SceModuleInfoInternal *pModuleInfo;
-
-	module_name[0x1B] = 0;
-	res_debug = sceKernelGetModuleInternalForKernel(modid, &pModuleInfo);
-	if(res_debug == 0){
-		strncpy(module_name, pModuleInfo->module_name, 0x1B);
-		// ksceDebugPrintf("[%-27s] flags:0x%X, shared:0x%X\n", pModuleInfo->module_name, pModuleInfo->flags, sceKernelGetModuleIsSharedByAddrForKernel(pid, pModuleInfo->segments[0].vaddr));
-	}
-
-	res = module_unload_for_pid(pid, modid, flags, option);
-	if(res_debug == 0){
-		// ksceDebugPrintf("Module Unload : [%-27s], modid:0x%X, res:0x%X\n", module_name, modid, res);
-	}else{
-		ksceDebugPrintf("Module Unload Error : 0x%X, 0x%X\n", res_debug, res);
-	}
-
-	return res;
-}
-
-tai_hook_ref_t create_new_module_class_ref;
-int create_new_module_class_patch(SceUID pid, int flags, SceModuleObject **dst){
-	return create_new_module_class(pid, flags, dst);
-}
-
-void hex_dump(const void *addr, int len){
-
-	if(addr == NULL)
-		return;
-
-	if(len == 0)
-		return;
-
-	for(int i=0;i<len;i+=0x10){
-		ksceDebugPrintf(
-			"%02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X %02X\n",
-			((char *)addr)[i + 0x0], ((char *)addr)[i + 0x1], ((char *)addr)[i + 0x2], ((char *)addr)[i + 0x3],
-			((char *)addr)[i + 0x4], ((char *)addr)[i + 0x5], ((char *)addr)[i + 0x6], ((char *)addr)[i + 0x7],
-			((char *)addr)[i + 0x8], ((char *)addr)[i + 0x9], ((char *)addr)[i + 0xA], ((char *)addr)[i + 0xB],
-			((char *)addr)[i + 0xC], ((char *)addr)[i + 0xD], ((char *)addr)[i + 0xE], ((char *)addr)[i + 0xF]
-		);
-	}
-}
-
-int write_file(const char *path, const void *data, SceSize size){
-
-	if(data == NULL || size == 0)
-		return -1;
-
-	SceUID fd = ksceIoOpen(path, SCE_O_WRONLY | SCE_O_CREAT | SCE_O_TRUNC, 0666);
-	if (fd < 0)
-		return fd;
-
-	ksceIoWrite(fd, data, size);
-	ksceIoClose(fd);
-
-	return 0;
-}
-
-int print_module_flags(SceUID pid){
-
-	int cpu_intr;
-	SceKernelProcessModuleInfo *pProcModuleInfo;
-	SceModuleInfoInternal *pModuleInfo;
-
-	pProcModuleInfo = getProcModuleInfo(pid, &cpu_intr);
-	if(pProcModuleInfo == NULL)
-		return -1;
-
-	resume_cpu_intr(pProcModuleInfo, cpu_intr);
-
-	pModuleInfo = pProcModuleInfo->pModuleInfo;
-
-	while(pModuleInfo != NULL){
-		ksceDebugPrintf("[%-27s] flags:0x%X\n", pModuleInfo->module_name, pModuleInfo->flags);
-		pModuleInfo = pModuleInfo->next;
-	}
-
-	return 0;
-}
-
-int module_testing_thread(SceSize args, void *argp){
-
-	SceUID modid;
-	SceUID shell_pid, shell_uid;
-
-	ksceKernelDelayThread(15 * 1000 * 1000);
-
-	shell_pid = ksceKernelSysrootGetShellPid();
-
-	shell_uid = search_module_by_name(shell_pid, "SceShell");
-
-	ksceDebugPrintf("shell_pid : 0x%X\n", shell_pid);
-	ksceDebugPrintf("shell_uid : 0x%X\n", shell_uid);
-
-	modid = module_load_for_pid(0x10005, "os0:/kd/enum_wakeup.skprx", 0, NULL);
-	ksceDebugPrintf("enum_wakeup.skprx modid : 0x%X\n", modid);
-
-	if(0){
-		SceUID SceSysmem_uid = search_module_by_name(0x10005, "SceSysmem");
-
-		SceKernelModuleInfo sce_info;
-		memset(&sce_info, 0, sizeof(SceKernelModuleInfo));
-
-		sceKernelGetModuleInfoForKernel(0x10005, SceSysmem_uid, &sce_info);
-
-		uint32_t *sysroot_func_table = *(uint32_t **)(sce_info.segments[1].vaddr + 0x75F8);
-
-		ksceDebugPrintf("SceSysrootForDriver_D75D4F37 : 0x%X\n", sysroot_func_table[0x368 >> 2]);
-	}
-
-	ksceDebugPrintf("enum_wakeup.skprx unload res : 0x%X\n", module_unload_for_pid(0x10005, modid, 0, NULL));
-	ksceDebugPrintf("\n");
-
-
-
-	write_file("uma0:syscall_table.bin", (void *)(*(int *)(SceKernelModulemgr_data + 0x334)), 0x4000);
-
-
-	ksceDebugPrintf("0x%X\n", *(int *)(SceKernelModulemgr_data + 0x338));
-
-
-	if(0){
-		print_module_flags(0x10005);
-		print_module_flags(shell_pid);
-	}
-
-	ksceDebugPrintf("Testing Thread Exit\n");
-
-	return ksceKernelExitDeleteThread(0);
-}
-
 void _start() __attribute__ ((weak, alias("module_start")));
 int module_start(SceSize args, void *argp){
 
@@ -1476,13 +1310,6 @@ int module_start(SceSize args, void *argp){
 		return SCE_KERNEL_START_SUCCESS;
 	}
 
-	HookOffset(modulemgr_uid, 0x5648, 1, create_new_module_class);
-
-	HookOffset(modulemgr_uid, 0x21EC, 1, module_load_for_pid);
-	HookOffset(modulemgr_uid, 0x26BC, 1, module_unload_for_pid);
-
-
-
 	SceKernelModuleInfo info;
 
 	sceKernelGetModuleInfoForKernel(0x10005, modulemgr_uid, &info);
@@ -1499,11 +1326,7 @@ int module_start(SceSize args, void *argp){
 
 	pThreadSomeInfo = (int *)(info.segments[1].vaddr + 0x44);
 
-	if(1){
-		SceUID thid = ksceKernelCreateThread("SceKernelModuleTestingThread", module_testing_thread, 0x60, 0x4000, 0, 0, NULL);
-		if(thid > 0)
-			ksceKernelStartThread(thid, 0, NULL);
-	}
+	my_debug_start();
 
 	return SCE_KERNEL_START_SUCCESS;
 }
