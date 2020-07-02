dd5a3f97032e4d398e94a2cb78fd14632bb3cf30
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-07-02 23:07:34 +0900

    Update import_defs.h

--- src/import_defs.h
+++ src/import_defs.h
@@ -9,8 +9,41 @@
 #include <psp2kern/kernel/sysmem.h>
 #include "modulemgr_internal.h"
 
+typedef struct SceSelfAuthInfo { // size is 0x90
+	SceUInt64 program_authority_id;
+	SceUInt64 padding1;
+	char capability[0x20];
+	char attributes[0x20];
+	uint8_t padding2[0x10];
+	uint8_t klicensee[0x10]; // offset 0x60
+	uint32_t unk_70;
+	uint32_t unk_74;
+	uint32_t unk_78;
+	uint32_t unk_7C;
+	uint32_t unk_80;
+	uint32_t unk_84;
+	uint32_t unk_88;
+	uint32_t unk_8C;
+} SceSelfAuthInfo;
+
+typedef struct SceSblSmCommContext130 { // size is 0x130 as its name indicates.
+	uint32_t unk_0;
+	uint32_t self_type;                    // kernel : 0 / user : 1 / main process : 0x10001
+
+	// offset:0x8
+	SceSelfAuthInfo self_auth_info_caller; // size is 0x90 - can be obtained with sceKernelGetSelfAuthInfoForKernel
+
+	// offset:0x98
+	SceSelfAuthInfo self_auth_info_called; // size is 0x90
+
+	// 0x128
+	uint32_t path_id;                      // can be obtained with sceSblACMgrGetPathIdForKernel or sceIoGetPathIdExForDriver
+	uint32_t unk_12C;
+} SceSblSmCommContext130;
+
 int __stack_chk_fail();
 
+int ksceIoGetMediaType(SceUID pid, const char *path, int ignored, int *media_type);
 SceUID ksceIoOpenForPid(SceUID pid, const char *path, int flags, int mode);
 
 int ksceKernelGetSystemTimeLow(void);
@@ -21,14 +54,14 @@ int ksceKernelCheckDipsw(int bit);
 int ksceMt19937GlobalUninit(void *a1, int a2);
 int ksceSblQafMgrIsAllowHost0Access(void);
 SceUID ksceKernelSysrootGetShellPid(void);
-int ksceKernelSysrootDbgpSuspendProcessAndWaitResume(SceUID pid, SceUID modid, int flags, int *a4, uint64_t time);
+int ksceKernelSysrootDbgpSuspendProcessAndWaitResume(SceUID pid, SceUID modid, int flags, uint64_t time);
 
 // SceSysmem
 SceClass *ksceKernelGetClassForUid(SceUID uid, SceClass **cls);
 int ksceKernelStrnlenUser(uintptr_t path, SceSize len);
 
 
-int sceKernelEnqueueWorkQueueForDriver(SceUID tid, const char *name, void *some_func, void *some_data);
+int sceKernelEnqueueWorkQueueForDriver(SceUID tid, const char *name, const void *func, void *argp);
 int SceThreadmgrForDriver_20C228E4(void);
 void *SceThreadmgrForDriver_3A72C6D8(int a1);
 
@@ -37,15 +70,33 @@ void *SceThreadmgrForDriver_3A72C6D8(int a1);
 int ksceKernelAllocSyscallTable(SceSize len, void *ptr_dst);
 
 
+int sceKernelGetProcessBudgetTypeForKernel(SceUID pid);
+
+int ksceKernelGetProcessAuthid(SceUID pid, uint64_t *pAuthid);
 
 void *sceKernelAllocRemoteProcessHeapForDriver(SceUID pid, int size, void *pOpt);
 void sceKernelFreeRemoteProcessHeapForDriver(SceUID pid, void *ptr);
 void *sceKernelGetProcessClassForKernel(void);
-int SceProcessmgrForKernel_0A5A2CF1(SceUID pid, int a2);
-int SceProcessmgrForKernel_B75FB970(SceUID pid, int a2);
-int SceProcessmgrForKernel_41815DF2(SceUID pid, void *a2);
-int SceProcessmgrForDriver_D141C076(SceUID uid, void *a2);
-SceKernelProcessModuleInfo *SceProcessmgrForKernel_C1C91BB2(SceUID pid);
+
+/*
+ * sceKernelProcessModuleInc
+ *
+ * a2 - if one, max 0x20 count increments, else max 0x7F count increments
+ */
+int sceKernelProcessModuleIncForKernel(SceUID pid, int a2);
+
+/*
+ * sceKernelProcessModuleDec
+ *
+ * a2 - if one, max 0x20 count decrements, else max 0x7F count decrements
+ */
+int sceKernelProcessModuleDecForKernel(SceUID pid, int a2);
+
+void *sceKernelProcessAllocKernelBudgetHeapMemoryForKernel(SceUID pid, SceSize len);
+int sceKernelFreeRemoteProcessKernelHeapForKernel(SceUID pid, void *ptr);
+SceKernelProcessModuleInfo *sceKernelGetProcessModuleInfoForKernel(SceUID pid);
+
+int sceKernelGetCompiledSdkVersionByPidForDriver(SceUID pid, unsigned int *pVersion);
 
 
 
@@ -53,18 +104,26 @@ void *ksceKernelAlloc(int size);
 int ksceKernelFree(void *ptr);
 int _ksceKernelCreateUidObj(SceClass *class, const char *name, SceCreateUidObjOpt *opt, SceObjectBase **obj_base);
 
-int SceQafMgrForDriver_382C71E8(void);
+int ksceKernelSetObjectForUid(SceUID uid, const char *name);
+
+int sceSblQafMgrIsAllowKernelDebugForDriver(void);
 
 int SceKernelSuspendForDriver_4DF40893(int a1);
 int SceKernelSuspendForDriver_2BB92967(int a1);
 
 void *_ksceKernelSysrootGetModulePrivate(int idx);
+
+// SceSysrootProcessHandler
 int SceSysrootForKernel_20D2A0DF(SceUID pid, SceUID modid, uint64_t time);
+int SceSysrootForKernel_C81B7E2B(SceUID pid, SceUID modid, uint64_t time);
+
+// SceSysrootDbgpHandler
 int SceSysrootForKernel_AA8D34EE(SceUID pid, SceUID modid, uint64_t time);
 int SceSysrootForKernel_CA497324(SceUID pid, SceUID modid, uint64_t time);
-int SceSysrootForKernel_C81B7E2B(SceUID pid, SceUID modid, uint64_t time);
 int SceSysrootForKernel_73522F65(SceUID pid, int *some_flag);
 int SceSysrootForKernel_6050A467(SceUID pid);
+
+// set state 2 to thread some storage
 void SceSysrootForDriver_6E0BC27C(void);
 
 int ksceKernelSysrootGetSystemSwVersion(void);
