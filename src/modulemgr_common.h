996ec725560b1ecae8b05cd5c01db1e3ebbc76df
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-07-02 23:12:05 +0900

    Update modulemgr_common.h

--- src/modulemgr_common.h
+++ src/modulemgr_common.h
@@ -17,8 +17,6 @@ int module_unload_for_pid(SceUID pid, SceUID modid, int flags, SceKernelULMOptio
 int module_stop_unload_for_pid(SceUID pid, SceUID modid, SceSize args, void *argp, int flags, SceKernelULMOption *option, int *status);
 SceUID module_load_start_shared_for_pid(SceUID pid, const char *path, SceSize args, void *argp, int flags, SceKernelLMOption *option, int *status);
 
-int create_new_module_class(SceUID pid, int flags, SceModuleObject **dst);
-
 void func_0x810014a8(void);
 void func_0x810014d4(void);
 
@@ -28,29 +26,24 @@ int func_0x810040c8(SceKernelProcessModuleInfo *a1);
 int func_0x81004198(void *a1, int a2, int a3);			// yet not Reversed
 int func_0x8100428c(void *a1, int a2, int a3);			// yet not Reversed
 
-int func_0x810049fc(const char *path);
-int func_0x81004a54(SceUID fd);
+SceUID sceIoOpenBootfs(const char *path);
+int sceIoCloseBootfs(SceUID fd);
 
-// int func_0x81005648(SceUID pid, int flags, void *dst);
-int func_0x81005648(SceUID pid, int flags, SceModuleObject **dst);
+int create_new_module_class(SceUID pid, int flags, SceModuleObject **dst);
 int get_export_index(SceModuleInfoInternal *pModuleInfo, const void *module_addr);
 
 void *func_0x81006cf4(int a1, void *a2, const void *a3, void *a4);
+void func_0x81006d40(void *a1, SceModuleInfoInternal *pModuleInfo);
 SceModuleLibraryObject *get_library_object(SceUID pid, SceUID library_id);
 
 int cleanup_process_module_info(SceUID pid);
 
 int func_0x810070b4(void *a1);
-int func_0x81007148(const char *path);
-int func_0x810071a8(void *r0);
 int get_module_library_info_export(SceUID pid, SceUID modid, uint32_t libnid, SceKernelLibraryInfo *info);
 int get_module_info(SceUID pid, SceUID modid, SceKernelModuleInfo_fix_t *info);
 int get_module_info_internal_by_addr(SceKernelProcessModuleInfo *module_proc_info, const void *module_addr, SceModuleInfoInternal **dst);
 
-int syacall_init(void);
-
-SceUID get_module_id_by_addr_internal(SceUID pid, const void *module_addr);
 SceUID get_module_id_by_addr(SceUID pid, const void *module_addr);
-SceUID search_module_by_name(SceUID pid, const char *module_name);
+SceUID get_module_id_by_addr_internal(SceUID pid, const void *module_addr);
 
 #endif /* _PSP2_KERNEL_MODULEMGR_COMMON_H_ */
