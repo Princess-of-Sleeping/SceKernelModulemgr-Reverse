07419181a930116d70cdf288754b4507937346e5
Princess of Sleeping <29831892+Princess-of-Sleeping@users.noreply.github.com>
2020-07-02 23:10:15 +0900

    Update module_debug.c

--- src/module_debug.c
+++ src/module_debug.c
@@ -32,7 +32,7 @@ const SceKernelDebugMessageContext load_param_one_seg = {
  * print_module_load_info / 0x81005b04 (checked)
  */
 void print_module_load_info(SceModuleInfoInternal *pModuleInfo){
-	if(SceQafMgrForDriver_382C71E8() != 0){
+	if(sceSblQafMgrIsAllowKernelDebugForDriver() != 0){
 		if(pModuleInfo->segments_num < 2){
 			ksceDebugPrintf2(0, &load_param_one_seg, "[%-27s]:text=%p(0x%08x), (no data)\n",
 				pModuleInfo->module_name, pModuleInfo->segments[0].vaddr, pModuleInfo->segments[0].memsz
